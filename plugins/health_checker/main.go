1a0447b0089d4a56baf7a302d6e9d989aba945b2
HeeJin Lee <heejin.lee@dsrvlabs.com>
2022-07-19 20:24:15 +0200

    Update send discord alert

--- plugins/health_checker/main.go
+++ plugins/health_checker/main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"time"
 	"flag"
+	"fmt"
+	"os"
+
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/robfig/cron/v3"
-    pluginpb "github.com/dsrvlabs/vatz-proto/plugin/v1"
-    "github.com/dsrvlabs/vatz/sdk"
-    "golang.org/x/net/context"
-    "google.golang.org/protobuf/types/known/structpb"
+
+	"github.com/dsrvlabs/vatz/manager/notification"
+	"github.com/dsrvlabs/vatz/manager/config"
+
+	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 )
@@ -17,16 +22,22 @@ type arrayFlags []string
 
 
 const (
-    defaultAddr = "127.0.0.1"
-    defaultPort = 9099
-    pluginName = "vatz-plugin-vatz-health-checker"
+	defaultAddr = "127.0.0.1"
+	defaultPort = 9090
+	defaultSchedule = "* * * * *"
 )
+
+type notificationInfo struct {
+	DiscordSecret	string
+	PagerDutySecret	string
+}
 
 var (
 	addr string
 	port int
 	schedule arrayFlags
-	ctx context.Context
+	dispatchManager	= notification.GetDispatcher()
+	configFile string
 )
 
 func (i *arrayFlags) String() string {
@@ -39,60 +50,72 @@ func (i *arrayFlags) Set(value string) error {
 }
 
 func init() {
-	flag.StringVar(&addr, "addr", defaultAddr, "Listening address")
-	flag.IntVar(&port, "port", defaultPort, "Listening port")
-	flag.Var(&schedule, "schedule", "Schedule with cron expression")
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339})
+	schedule.Set(defaultSchedule)
 
+	flag.StringVar(&addr, "addr", defaultAddr, "remote address")
+	flag.IntVar(&port, "port", defaultPort, "remote port")
+	flag.Var(&schedule, "schedule", "Schedule with cron expression, use UTC timezone, space separated ex.) \"* * * * *\"")
+	flag.StringVar(&configFile, "config", "default.yaml", "config file path")
 	flag.Parse()
+
+	config.InitConfig(configFile)
+
 }
 
 func main() {
-    p := sdk.NewPlugin(pluginName)
-    p.Register(pluginFeature)
+	cfg := config.GetConfig()
+	vatzConfig := cfg.Vatz
 
-    ctx = context.Background()
-
+	n := notificationInfo{DiscordSecret: vatzConfig.NotificationInfo.DiscordSecret}
+	raddr := fmt.Sprint(addr, ":",  port)
 	c := cron.New(cron.WithLocation(time.UTC))
+
 	for i := 0; i < len(schedule); i++ {
-		log.Info().Str("module", "plugin").Msgf("%d, %s", i, schedule[i])
-		c.AddFunc(schedule[i], func() { healthCheck("localhost:9090", ctx) })
+		log.Debug().Str("module", "plugin").Msgf("Add cronjob %d, %s", i, schedule[i])
+		c.AddFunc(schedule[i], func() { healthCheck(raddr, n.DiscordSecret, context.Background()) })
 	}
 
 	c.Start()
-	//healthCheck("localhost:9090", ctx)
-    if err := p.Start(ctx, addr, port); err != nil {
-        log.Info().Str("module", "plugin").Msg("exit")
-    }
+
+	for {
+	}
 }
 
-func healthCheck(address string, ctx context.Context) {
+func healthCheck(address string, webhook string, ctx context.Context) {
+	jsonMessage := notification.ReqMsg{
+		FuncName:	"is_vatz_up",
+		State:		notification.Success,
+		Msg:		"vatz is Alive!!",
+		Severity:	notification.Info,
+		ResourceType:	"vatz_health_checker",
+	}
+
 	log.Info().Str("module", "plugin").Msgf("dial to %v", address)
 	conn, err := grpc.Dial(address, grpc.WithInsecure())
 
 	if err != nil {
 		log.Error().Str("module", "plugin").Msg("dial error")
 	}
-	log.Info().Str("module", "plugin").Msg(conn.GetState().String())
 
 	healthClient := healthpb.NewHealthClient(conn)
 	response, err := healthClient.Check(ctx, &healthpb.HealthCheckRequest{})
+
 	if err != nil {
 		log.Error().Str("module", "plugin").Msg("health check response error")
+		jsonMessage = notification.ReqMsg{
+			State:		notification.Faliure,
+			Msg:		"vatz is down !!",
+			Severity:	notification.Critical,
+		}
+
+		dispatchManager.SendDiscord(jsonMessage, webhook)
+		log.Info().Str("module", "plugin").Msgf("%v", response)
+
+		return
 	}
 
+	dispatchManager.SendDiscord(jsonMessage, webhook)
+
 	log.Info().Str("module", "plugin").Msgf("%v", response)
-}
-
-func pluginFeature(info, option map[string]*structpb.Value) (sdk.CallResponse, error) {
-    // TODO: Fill here.
-    ret := sdk.CallResponse{
-        FuncName:   "vatzHealthChecker",
-        Message:    "vatz alive!",
-        Severity:   pluginpb.SEVERITY_UNKNOWN,
-        State:      pluginpb.STATE_NONE,
-        AlertTypes: []pluginpb.ALERT_TYPE{pluginpb.ALERT_TYPE_DISCORD},
-    }
-
-	healthCheck("localhost:9090", ctx)
-    return ret, nil
 }
