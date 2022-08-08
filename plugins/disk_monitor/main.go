4eb081587219841102a380a4d2ac18c40e09913e
HeeJin Lee <heejin.lee@dsrvlabs.com>
2022-08-08 08:16:07 +0200

    Update disk monitor
    add path params to check disk usage.

--- plugins/disk_monitor/main.go
+++ plugins/disk_monitor/main.go
@@ -12,12 +12,15 @@ import (
     "github.com/shirou/gopsutil/v3/disk"
 )
 
+type arrayFlags []string
+
 const (
     defaultAddr = "127.0.0.1"
     defaultPort = 9096
     pluginName = "vatz-plugin-solana-disk-monitor"
     defaultUrgent = 95
     defaultWarning = 90
+	defaultPath = "/"
 )
 
 var (
@@ -25,13 +28,25 @@ var (
     warning int
     addr string
     port int
+	mountPaths arrayFlags
 )
 
+func (i *arrayFlags) String() string {
+	return ""
+}
+
+func (i *arrayFlags) Set(value string) error {
+	*i = append(*i, value)
+	return nil
+}
+
 func init() {
+	mountPaths.Set(defaultPath)
     flag.StringVar(&addr, "addr", defaultAddr, "Listening address")
     flag.IntVar(&port, "port", defaultPort, "Listening port")
     flag.IntVar(&urgent, "urgent", defaultUrgent, "Disk Usage Alert threshold")
     flag.IntVar(&warning, "warning", defaultWarning, "Disk Usage Warning threshold")
+	flag.Var(&mountPaths, "paths", "mount paths to check available size (default \"/\")")
 
     flag.Parse()
 }
@@ -55,35 +70,29 @@ func pluginFeature(info, option map[string]*structpb.Value) (sdk.CallResponse, e
         State:      pluginpb.STATE_NONE,
         AlertTypes: []pluginpb.ALERT_TYPE{pluginpb.ALERT_TYPE_DISCORD},
     }
+	log.Info().Str("module", "plugin").Int("length:", len(mountPaths)).Msg("disk_monitor")
+	for i := 0; i < len(mountPaths); i++ {
+		log.Info().Str("module", "plugin").Msgf("path %d %s", i, mountPaths[i])
+	}
+	used := make([]int, len(mountPaths))
+	for i := 0; i < len(mountPaths); i++ {
+		temp, _ := disk.Usage(mountPaths[i])
+		used[i] = int(temp.UsedPercent)
+		log.Debug().Str("module", "plugin").Int("", used[i]).Int("Urgent", urgent).Int("Warning", warning).Msgf("disk_monitor: Disk Usage of %s", mountPaths[i])
+		if used[i] > urgent {
+			var message string
+			message = fmt.Sprint("Current Disk Usage of ", mountPaths[i], used[i], "%, over urgent threshold ", urgent, "%")
+			ret = sdk.CallResponse{
+				FuncName:	"getDISKUsgae",
+				Message:	message,
+				Severity:	pluginpb.SEVERITY_CRITICAL,
+				State:		pluginpb.STATE_SUCCESS,
+				AlertTypes:	[]pluginpb.ALERT_TYPE{pluginpb.ALERT_TYPE_DISCORD},
+			}
 
-    root, _ := disk.Usage("/")
-    log.Debug().Str("module", "plugin").Int("Disk Usage (/):", int(root.UsedPercent)).Int("Urgent", urgent).Int("Warning", warning).Msg("disk_monitor")
-
-    mnt, _ := disk.Usage("/mnt/solana")
-    log.Debug().Str("module", "plugin").Int("Disk Usage (/mnt/solana):", int(mnt.UsedPercent)).Int("Urgent", urgent).Int("Warning", warning).Msg("disk_monitor")
-    // TODO: grep mountinfo and check disk usage for each mount point
-
-    if int(root.UsedPercent) > urgent {
-        var message string
-        message = fmt.Sprint("Current Disk Usage (/): ", int(root.UsedPercent), "%, over urgent threshold ", urgent, "%")
-        ret = sdk.CallResponse{
-            Message:	message,
-            Severity:	pluginpb.SEVERITY_CRITICAL,
-        }
-
-        log.Warn().Str("module", "plugin").Msg(message)
-    }
-
-    if int(mnt.UsedPercent) > urgent {
-        var message string
-        message = fmt.Sprint("Current Disk Usage (/mnt/solana): ", int(mnt.UsedPercent), "%, over urgent threshold ", urgent, "%")
-        ret = sdk.CallResponse{
-            Message:	message,
-            Severity:	pluginpb.SEVERITY_CRITICAL,
-        }
-
-        log.Warn().Str("module", "plugin").Msg(message)
-    }
+			log.Warn().Str("module", "plugin").Msg(message)
+		}
+	}
 
     return ret, nil
 }
